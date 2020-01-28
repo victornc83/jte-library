ded2920b2c2700645f703358b7fabb9b0f2ddfcb
Victor Manuel Nieto Castan <vnietoca@unicaja.es>
2020-01-28 21:33:49 +0100

    cambio

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -1,9 +1,6 @@
 void call(){
     stage("Maven: Build"){
         println "build from the maven library"
-        withMaven(){
-            echo "${POM_DISPLAYNAME}"
-            sh "mvn package -DskipTests"
-        }
+        sh "mvn package -DskipTests"
     }
 }
